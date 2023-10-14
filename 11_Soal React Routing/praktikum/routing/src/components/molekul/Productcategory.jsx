98a1d660a153779cd904f8b3b8bdbd54fb811677
RAMA-AGI-PRATAMA <ramaagi18@gmail.com>
2023-10-14 21:40:42 +0700

    Membuat LandingPage dengan Routing

---
+++ 11_Soal React Routing/praktikum/routing/src/components/molekul/Productcategory.jsx
@@ -0,0 +1,17 @@
+import Label from "../atom/Label";
+
+const Productcategory = ({name,title, className, value, onChange}) => {
+    return (
+    <div className="d-flex flex-column mt-4" style={{width:'20%'}}>
+        <Label htmlFor={name} title={title}/>
+        <select className={className} name={name} id={name} value={value} onChange={onChange}>
+            <option value="Electronics">Electronics</option>
+            <option value="Furniture">Furniture</option>
+            <option value="Clothing">Clothing</option>
+            <option value="Sport">Sport</option>
+            <option value="Gadget">Gadget</option>
+        </select>
+    </div>
+    )
+}
+export default Productcategory
