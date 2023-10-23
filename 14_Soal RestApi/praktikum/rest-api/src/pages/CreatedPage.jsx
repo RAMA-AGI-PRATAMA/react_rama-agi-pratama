f5dcee74537a1f8022c1ac818e51ee65f45bb005
RAMA-AGI-PRATAMA <ramaagi18@gmail.com>
2023-10-23 16:22:48 +0700

    Mengganti Data Create Product  Menggunakan MockAPI

---
+++ 14_Soal RestApi/praktikum/rest-api/src/pages/CreatedPage.jsx
@@ -0,0 +1,19 @@
+import FormData from '../components/organisms/FormData'
+import React from 'react'
+import Tagline from '../components/organisms/Tagline'
+import Editing from '../components/atom/Modal'
+
+const CreatedPage = () => {
+    return (
+        <>
+        <div className='container mt-5'>
+        <div className='row justify-content-center'>
+            <Tagline />
+            <FormData />
+        </div>
+        </div>
+
+        </>
+    )
+}
+export default CreatedPage
